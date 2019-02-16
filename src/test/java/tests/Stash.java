62a2cf0c689b4227a66eb613c73379aa6325d959
gulmira168 <46835460+gulmira168@users.noreply.github.com>
2019-02-15 21:57:13 -0500

    stash 2 is created

--- src/test/java/tests/Stash.java
+++ src/test/java/tests/Stash.java
@@ -4,5 +4,9 @@ public class Stash {
     public void Stash2(){
         System.out.println("stash 2");
     }
+    
+        public void Stash3(){
+        System.out.println("stash 2");
+    }
 
 }
