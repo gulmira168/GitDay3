115f1bc97fe407e682ef047c32cf4697961f7077
gulmira168 <gulmira.alchikeeva@gmail.com>
2019-02-15 21:41:12 -0500

    stashing

---
+++ src/test/java/tests/Stash.java
@@ -0,0 +1,8 @@
+package tests;
+
+public class Stash {
+    public void Stash2(){
+        System.out.println("stash 2");
+    }
+
+}
---
+++ src/test/java/tests/StashClass.java
@@ -0,0 +1,8 @@
+package tests;
+
+public class StashClass {
+
+    public void StashClass(){
+        System.out.println("stash 1");
+    }
+}
